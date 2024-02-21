36bf20844f889232f781aabcad4d10e8f5f916d0
Lukas Plank <lupl@tuta.io>
2024-02-21 09:47:11 +0100

    Implemented public package interface

---
+++ lodkit/__init__.py
@@ -0,0 +1,9 @@
+from lodkit.utils import ttl, genhash
+from lodkit.graph import Graph
+from lodkit.types import (
+    _TripleObject,
+    _Triple,
+    _TripleObjectLiteral,
+    _TripleObjectURI,
+    _GraphFormatOptions
+)
