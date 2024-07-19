43d4b5c5f979eb944b286746a2a19b631bd22b05
Lukas Plank <lupl@tuta.io>
2024-07-19 18:06:35 +0200

    Implemented ClosedOntologyNamespace tests

--- tests/test_namespace_tools/test_closed_ontology_namespace.py
+++ tests/test_namespace_tools/test_closed_ontology_namespace.py
@@ -1,22 +1,26 @@
 """"""
 
-from collections.abc import Iterator
 from importlib.resources.abc import Traversable
 
-import pytest
-
-from hypothesis import given, settings
-from tests.utils.strategies.ns_strategies import public_variable_names
+from hypothesis import given, strategies as st
 from lodkit import ClosedOntologyNamespace
-from lodkit.namespace_tools.ontology_namespaces import (
+from lodkit.namespace_tools._exceptions import NamespaceDelimiterException
+from lodkit.namespace_tools.utils import (
+    MultiOntologyHeadersException,
+    NoOntologyHeaderException,
     _get_namespace_from_ontology,
     _get_terms_from_ontology,
 )
-from rdflib import Graph
-from tests.utils.paths import ontologies_path
+import pytest
+from rdflib import Graph, Namespace
+from tests.utils.paths import fail_ontologies_path, ontologies_path
+from tests.utils.strategies.ns_strategies import public_variable_names
+from tests.utils.utils import parametrize_paths_from_glob
 
 
-_ontologies: list[Traversable] = list(ontologies_path.iterdir())
+_ontologies: list[Traversable] = list(
+    obj for obj in ontologies_path.iterdir() if obj.is_file()
+)
 
 
 @pytest.mark.parametrize("ontology_path", _ontologies)
@@ -54,7 +58,45 @@ def test_closed_ontology_namespace_unknown_term_fail(ontology_path):
         getattr(closed_ontology_namespace, "does_not_exist")
 
 
-# note: just realized that _TGraphOrPath is too strict because Graph.parse also takes URLs
-# either update _TGraphOrPath to _TGraphParsable or copy type from rdflib.parse source parameter
-# @given()
-# def test_closed_ontology_namespace_reference_exception(ontology_path):
+@parametrize_paths_from_glob(
+    fail_ontologies_path / "delimiter_fails", param="fail_ontology"
+)
+def test_closed_ontology_namespace_delimiter_side_effects_exception(fail_ontology):
+    """Check if an undelimited namespace raises an exception if strict_delimiters=True."""
+
+    with pytest.raises(NamespaceDelimiterException):
+        closed_ontology_namespace = ClosedOntologyNamespace(
+            fail_ontology, strict_delimiters=True
+        )
+
+
+@parametrize_paths_from_glob(
+    fail_ontologies_path / "delimiter_fails", param="fail_ontology"
+)
+def test_closed_ontology_namespace_delimiter_side_effects_warning(
+    fail_ontology, caplog
+):
+    """Check if an undelimited namespace emits a warning if strict_delimiters=False."""
+    closed_ontology_namespace = ClosedOntologyNamespace(
+        fail_ontology, strict_delimiters=False
+    )
+    log_message = (
+        f"The derived Ontology namespace '{closed_ontology_namespace.uri}' "
+        "does not feature a common URI entity delimiter (#, /)."
+    )
+
+    assert log_message in caplog.text
+
+
+def test_closed_ontology_multi_header_fail():
+    """Check if a MultiOntologyHeadersException is raised."""
+    fail_ontology_path = fail_ontologies_path / "header_fails/multiple_headers.ttl"
+    with pytest.raises(MultiOntologyHeadersException):
+        closed_ontology_namespace = ClosedOntologyNamespace(fail_ontology_path)
+
+
+def test_closed_ontology_no_header_fail():
+    """Check if a NoOntologyHeadersException is raised."""
+    fail_ontology_path = fail_ontologies_path / "header_fails/no_header_cidoc.ttl"
+    with pytest.raises(NoOntologyHeaderException):
+        closed_ontology_namespace = ClosedOntologyNamespace(fail_ontology_path)
