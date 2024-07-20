7f4b04a2e981ca906e645480c8b0e0195a5da4ed
Lukas Plank <lupl@tuta.io>
2024-07-20 08:48:40 +0200

    Added uribase instantiation fail test

--- tests/test_uri_tools/test_uribase.py
+++ tests/test_uri_tools/test_uribase.py
@@ -4,6 +4,8 @@ from types import new_class
 
 from hypothesis import given, strategies as st
 from lodkit import mkuri_factory, uribase
+from lodkit.uri_tools.uribase import InstantiationException
+import pytest
 from rdflib import Namespace, URIRef
 from tests.utils.strategies.ns_strategies import public_variable_names
 
@@ -36,3 +38,19 @@ def test_uribase_hashed(variable_name, hash_value):
     )
 
     assert getattr(uribase_cls, variable_name) == mkuri(hash_value)
+
+
+@given(public_variable_names, st.text())
+def test_uribase_instantiation_fail(variable_name, hash_value):
+    """Check if uribase and mkuri_factory constructed hashed URIs are equal."""
+    namespace = Namespace("https://lodkit.testing/")
+    mkuri = mkuri_factory(namespace)
+
+    uribase_cls = new_class(
+        "uribase_cls",
+        bases=(uribase(namespace),),
+        exec_body=lambda ns: ns.update({variable_name: hash_value}),
+    )
+
+    with pytest.raises(InstantiationException):
+        uribase_cls()
