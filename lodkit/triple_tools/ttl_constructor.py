57f938b6157f114840e358668d038521ac37c7ab
Lukas Plank <lupl@tuta.io>
2025-11-23 09:33:37 +0100

    feat!: implement RDF Collections in lodkit.ttl
    The commit introduces changes to the lodkit.ttl constructor:
    1. a new syntax for Turtle object lists is introduces and
    2. support for RDF Collections is added.
    
    @1
    Previously, Turtle object lists were indicated in lodkit.ttl by
    tuple objects, e.g. ttl(s, (p, (o1, o2, o3))). With the change
    introduced here, the predicate-object pair of lodkit.ttl can be of
    arbitrary length where the first element is interpreted as predicate
    and all other elements are interpreted as members of a Turtle object
    list; the above example expressed with the new syntax looks like this:
    ttl(s, (p, o1, o2, o3)).
    
    @2
    RDF Collections are now supported in lodkit.ttl and are indicated with
    a tuple object (previously the indicator for object lists);
    e.g ttl(s, (p, (o1, o2, o3))) now gets resolved as
    "s p (o1 o2 o3)" in Turtle.

--- lodkit/triple_tools/ttl_constructor.py
+++ lodkit/triple_tools/ttl_constructor.py
@@ -4,88 +4,68 @@ from collections.abc import Iterable, Iterator
 from itertools import repeat
 
 from lodkit.lod_types import _Triple, _TripleObject, _TripleSubject
-from rdflib import BNode, Graph, Literal, URIRef
+from rdflib import BNode, Graph, Literal, RDF, URIRef
 
 
 type _TPredicateObjectPairObject = (
     _TripleObject
     | str
     | list[_TPredicateObjectPair]
-    | Iterator[_TPredicateObjectPair]
     | tuple[_TPredicateObjectPairObject, ...]
     | ttl
 )
 
-type _TPredicateObjectPair = tuple[URIRef, _TPredicateObjectPairObject]
+type _TPredicateObjectPair = tuple[URIRef, *tuple[_TPredicateObjectPairObject, ...]]
 
 
 class ttl(Iterable[_Triple]):
-    """Triple generation facility that implements a Turtle-like interface.
-
-    The generator takes a triple subject and an aribitrary number of predicate/object pairs
-    and produces an Iterator of RDFLib object 3-tuples.
-
-    Triple objects passed to the constructor can be
-    - URIRefs, BNodes, Literals
-    - Python lists of predicate/object tuples (resolved as blank nodes),
-    - tuples (resolved as Turtle object lists)
-    - ttl constructors (resolved recursively)
-
-    Args:
-        uri (_TripleSubject): The subject of a triple
-        *predicate_object_pairs (tuple[ URIRef, _TripleObject | list | Iterator | Self | str | tuple[_TripleObject, ...]]): Predicate-object pairs
-
-    Returns:
-        None
-
-    Examples:
-
-        triples: Iterator[lodkit._Triple] = ttl(
-            URIRef('https://subject'),
-            (RDF.type, URIRef('https://some_type')),
-            (RDFS.label, Literal('label 1'), 'label 2'),
-            (RDFS.seeAlso, [(RDFS.label, 'label 3')]),
-            (RDFS.isDefinedBy, ttl(URIRef('https://subject_2'), (RDF.type, URI('https://another_type'))))
-        )
-
-        graph: Graph = triples.to_graph()
-    """
+    """Triple generation facility that implements a Turtle-like interface."""
 
     def __init__(
         self,
-        uri: _TripleSubject,
+        subject: _TripleSubject,
         *predicate_object_pairs: _TPredicateObjectPair,
     ) -> None:
-        self.uri = uri
+        self.subject = subject
         self.predicate_object_pairs = predicate_object_pairs
 
     def __iter__(self) -> Iterator[_Triple]:
         """Generate an iterator of 3-tuple triple representations."""
 
-        for pred, obj in self.predicate_object_pairs:
-            match obj:
-                case ttl():
-                    yield (self.uri, pred, obj.uri)
-                    yield from obj
-                case list() | Iterator():
-                    _b = BNode()
-                    yield (self.uri, pred, _b)
-                    yield from ttl(_b, *obj)
-                case tuple():
-                    _object_list = zip(repeat(pred), obj)
-                    yield from ttl(self.uri, *_object_list)
-                case obj if isinstance(obj, _TripleObject):
-                    yield (self.uri, pred, obj)
-                case str():
-                    yield (self.uri, pred, Literal(obj))
-                case _:
-                    raise TypeError(
-                        f"Unable to process triple object '{obj}'. "
-                        "See the ttl docs and type annotation for applicable object types."
-                    )
+        for pred, *objs in self.predicate_object_pairs:
+            for obj in objs:
+                match obj:
+                    case ttl():
+                        yield (self.subject, pred, obj.subject)
+                        yield from obj
+                    case list():
+                        _b = BNode()
+                        yield (self.subject, pred, _b)
+                        yield from ttl(_b, *obj)
+                    case tuple():
+                        first, *rest = obj
+                        yield from ttl(
+                            self.subject,
+                            (
+                                pred,
+                                [
+                                    (RDF.first, first),
+                                    (RDF.rest, tuple(rest) or RDF.nil),
+                                ],
+                            ),
+                        )
+                    case obj if isinstance(obj, _TripleObject):
+                        yield (self.subject, pred, obj)
+                    case str():
+                        yield (self.subject, pred, Literal(obj))
+                    case _:
+                        raise TypeError(
+                            f"Unable to process triple object '{obj}'. "
+                            "See the ttl docs and type annotation for applicable object types."
+                        )
 
     def to_graph(self, graph: Graph | None = None) -> Graph:
-        """Generate a graph instance from a ttl Iterator."""
+        """Generate a graph instance from a ttl object."""
         _graph = Graph() if graph is None else graph
 
         for triple in self:
