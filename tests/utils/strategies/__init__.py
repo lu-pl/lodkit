9481f642fe9baeb5a62ad7d37d8ffa07caa2dcd5
Lukas Plank <lupl@tuta.io>
2025-11-28 17:07:21 +0100

    chore: run ruff formatter

--- tests/utils/strategies/__init__.py
+++ tests/utils/strategies/__init__.py
@@ -1,2 +1,2 @@
-from tests.utils.strategies.ns_strategies import * # noqa: F403, F405
-from tests.utils.strategies.ttl_strategies import * # noqa: F403, F405
+from tests.utils.strategies.ns_strategies import *  # noqa: F403, F405
+from tests.utils.strategies.ttl_strategies import *  # noqa: F403, F405
