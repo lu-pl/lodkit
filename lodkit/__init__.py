799ad3c53dfb2c6db75fc5f8a1f645c5c95ff0b0
Lukas Plank <lupl@tuta.io>
2025-11-28 18:18:25 +0100

    feat: expose lodkit.ttl types in public library interface

--- lodkit/__init__.py
+++ lodkit/__init__.py
@@ -24,7 +24,11 @@ from lodkit.namespace_tools.ontology_namespaces import (
     DefinedOntologyNamespace,
 )
 from lodkit.rdf_importer import RDFImporter
-from lodkit.triple_tools.ttl_constructor import ttl
+from lodkit.triple_tools.ttl_constructor import (
+    _TPredicateObjectPair,
+    _TPredicateObjectPairObject,
+    ttl,
+)
 from lodkit.uri_tools.uribase import uribase
 from lodkit.uri_tools.uriclass import make_uriclass, uriclass
 from lodkit.uri_tools.utils import (
