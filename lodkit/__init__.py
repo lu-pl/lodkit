3a8c72f32a22198ad0c956da1cfb37cd06cc4dcd
Lukas Plank <lupl@tuta.io>
2025-12-01 10:59:08 +0100

    feat: implement TripleChain class
    The commit introduces an itertools.chain subclass, TripleChain, which
    allows to chain Iterables of lodkit._Triples and construct a graph
    from it.

--- lodkit/__init__.py
+++ lodkit/__init__.py
@@ -25,6 +25,7 @@ from lodkit.namespace_tools.ontology_namespaces import (
 )
 from lodkit.rdf_importer import RDFImporter
 from lodkit.testing_tools.strategies import TripleStrategies, tst, tst_xml
+from lodkit.triple_tools.triple_chain import TripleChain
 from lodkit.triple_tools.ttl_constructor import ttl
 from lodkit.uri_tools.uribase import uribase
 from lodkit.uri_tools.uriclass import make_uriclass, uriclass
